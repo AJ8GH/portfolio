c9a272aaf4c4d5bf1ee7aff0e57af3c583ff3d3b
Adam Jonas <adamjonas25@gmail.com>
2021-05-01 17:22:37 +0100

    Implement initial test, make styles dir in src

--- cypress/integration/home_page.spec.js
+++ cypress/integration/home_page.spec.js
@@ -1,1 +1,5 @@
-describe('')
+describe('Home page', () => {
+  it('Displays my name', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import '..public/css/App.css'
+import './styles/App.css'
 
 export default function App () {
   return (
