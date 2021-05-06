fd1a22f8fe6fc328c81381a2edc228dda303f502
Adam Jonas <adamjonas25@gmail.com>
2021-05-06 21:48:13 +0100

    Add color scheme and make profile llinks black

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ export default function App () {
 
           <div className="intro">
             <p>Welcome to my portfolio</p><br></br>
-            <p>I am a Junior Software Engineer with passion, curioristy and a strong desire to learn. I get excited about writing high quality code, practiscing test driven development, and building awesome apps and products.</p><br></br>
-            <p>Enjoy looking through my work and please feel free to contact me, download my CV or checkout my Github and linkedIn profiles</p>
+            <p>I am a Junior Software Engineer with passion, curiosity and a strong desire to learn. I get excited about writing high quality code, practising test driven development, and building awesome apps and products.</p><br></br>
+            <p>Enjoy looking through my work and please feel free to contact me, download my CV or checkout my GitHub and linkedIn profiles</p>
           </div>
         </header>
 
