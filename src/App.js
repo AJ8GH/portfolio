f84400910c561370494615bc294fb7df478dd5b8
Adam Jonas <adamjonas25@gmail.com>
2021-05-06 18:08:12 +0100

    Resize portrait

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import ContactLinks from './ContactLinks'
+import './styles/App.css'
 
 export default function App () {
   return (
     <div className="App">
-      <ContactLinks />
-      <header className="App-header">
-        <h1>
-          Adam Jonas
-        </h1>
+      <div className="container">
+        <ContactLinks />
+        <header className="App-header">
+          <h1>Adam Jonas</h1>
 
-        <p>Welcome to my portfolio</p>
-        <p>I am a passionate and curious engineer, excited by building products with high quality code</p>
-        <p>Please enjoy looking through my work and contact me if you have any questions at all!</p>
-      </header>
+          <div className="intro">
+            <p>Welcome to my portfolio</p>
+            <p>I am a passionate and curious engineer, excited by building products with high quality code</p>
+            <p>Please enjoy looking through my work and contact me if you have any questions at all!</p>
+          </div>
+        </header>
 
-      <img src={process.env.PUBLIC_URL + '/portrait.JPG'} className="portrait" alt="Image of Adam" />
+        <img src={process.env.PUBLIC_URL + '/portrait.JPG'} className="portrait" alt="Image of Adam" />
+      </div>
     </div>
   )
 }
